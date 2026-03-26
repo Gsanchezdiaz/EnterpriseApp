2b87ddf2eaaad41b240ed53c20cbcfe0c2ad2ee4
Gerson Sanchez Diaz <gsanchez@x-pertec.com>
2026-03-26 01:00:37 -0500

    feat: agregar configuracion shadcn

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,12 @@
+import { Button } from "@/components/ui/button";
 import prisma from "@/lib/prisma";
 
 export default async function Home() {
   const user = await prisma.user.findFirst();
   return (
-    <div>Hola Mundo: {JSON.stringify(user)}</div>
+    <div>
+      <h1>Hola Mundo: {JSON.stringify(user)}</h1>
+      <Button>Okis</Button>
+    </div>
   );
 }
